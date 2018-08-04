14043680e6c139125ec3e9bb9405cc5ae2182d04
sophia <sophiabaldonado@gmail.com>
2018-08-04 14:07:25 -0700

    opps lol;

--- main.lua
+++ main.lua
@@ -1,28 +1,63 @@
+flux = require 'flux'
+wave = require 'wave'
+
 function lovr.load()
 	shader = require('lighting')()
 	lovr.graphics.setShader(shader)
+	music = wave:newSource('song2.ogg', 'stream')
+	music:setBPM(170)
+	song = lovr.audio.newSource('song2.ogg')
+	song:play()
+	-- music:parse()
+	-- music:setIntensity(20)
+	balls = {}
+	balls.size = .2
+	runningTime = 0
 end
 
 function lovr.update(dt)
+	flux.update(lovr.timer.getDelta())
+	music:update(dt)
 
+	-- music:onBeat(function()
+	-- 	pulseBalls()
+	-- end)
+
+	local t = lovr.timer.getTime()
+	if (t - runningTime) > .345 then
+		runningTime = t
+		pulseBalls()
+	end
 end
 
 function lovr.draw()
-	local time = lovr.timer.getTime()
+	-- lovr.graphics.setWireframe(true)
 
+	defaultMovement()
+end
+
+function defaultMovement()
+	local time = lovr.timer.getTime()
 	for dep = -5, 5 do
 		for col = -2, 2 do
 			for row = 0, 3 do
 				local r, g, b = hsv(.08 * (row + col + dep) + lovr.timer.getTime() * .5, .5, 1)
 				local y = row + (math.sin(time * 3 + col + (row * .9) + select(3, lovr.headset.getPosition())) * .075)
 				local z = dep + -(math.cos(time * 3 + col + (row * .9) + select(3, lovr.headset.getPosition())) * .075)
-				local size = .2 + (math.sin(time + col + row) * .05)
+				-- balls.size = balls.size-- + (math.sin(time + col + row) * .05)
 
 				lovr.graphics.setColor(r, g, b)
-				lovr.graphics.sphere(col, y, z, size, 0, 0, 0, 0, 10)
+				lovr.graphics.sphere(col, y, z, balls.size, 0, 0, 0, 0, 10)
 			end
 		end
 	end
+end
+
+function pulseBalls()
+	local oSize = balls.size
+	local nSize = balls.size * 1.15
+	flux.to(balls, 0.1, { size = nSize })
+	flux.to(balls, 0.1, { size = oSize }):delay(0.05)
 end
 
 function hsv(h, s, v)
